a45fd34202a01369d8fb1596cdb8be98eeb36848
James Christie <james.aaron.christie@gmail.com>
2015-07-27 17:22:08 +0000

    Pass the seventh test

--- prime_factors/prime_factorer.py
+++ prime_factors/prime_factorer.py
@@ -1,20 +1,24 @@
 class PrimeFactorer:
   def __init__(self, factorable):
     self.factorable = factorable
-    self.primes     = None
+    self.primes     = []
 
   def generate(self):
-    self.primes = []
+    factor_candidate = 2
 
-    while self.__two_is_factor():
-      self.primes.append(2)
-      self.factorable = int(self.factorable / 2)
+    while self.factorable > 1:
+      while self.__two_is_factor(factor_candidate):
+        self.primes.append(factor_candidate)
+        self.factorable = int(
+          self.factorable / factor_candidate
+        )
+      factor_candidate += 1
 
     if self.factorable > 1:
       self.primes.append(self.factorable)
 
   # Private
 
-  def __two_is_factor(self):
-    remainder = self.factorable % 2
+  def __two_is_factor(self, factor_candidate):
+    remainder = self.factorable % factor_candidate
     return remainder == 0
