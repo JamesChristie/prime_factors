27ccecf883d99c3c3793849d7a283c9d01b763c8
James Christie <james.aaron.christie@gmail.com>
2015-07-27 16:44:50 +0000

    Failing second test

--- tests/test_prime_factorer.py
+++ tests/test_prime_factorer.py
@@ -9,3 +9,11 @@ class TestPrimeFactorerForOne(unittest.TestCase):
 
   def test_one(self):
     self.assertEqual(self.factorer.result, [])
+
+class TestPrimeFactorerForTwo(unittest.TestCase):
+  def setUp(self):
+    self.factorer = PrimeFactorer(2)
+    self.factorer.generate()
+
+  def test_two(self):
+    self.assertEqual(self.factorer.result, [2])
